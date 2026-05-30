e5d045e5c03b1b1adebb9691edba0ca0b8e8435a
bpant <brightpants97@gmail.com>
2026-05-31 06:06:32 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,7 @@
 import type { NextConfig } from "next";
 
-const BJID = process.env.SOOP_BJID || 'townboy';
-
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        // /api/soop-station 요청을 SOOP 방송국 페이지로 중계
-        source: '/api/soop-station',
-        destination: `https://www.sooplive.com/station/${BJID}`,
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
