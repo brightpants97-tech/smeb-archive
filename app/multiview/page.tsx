bf4caea2412a4c4e8fa09b0094f75ca698a9c4bf
bpant <brightpants97@gmail.com>
2026-06-19 00:34:40 +0900

    fix: 메타 설명 최대 4개로 수정

--- app/multiview/page.tsx
+++ app/multiview/page.tsx
@@ -3,7 +3,7 @@ import MultiviewClient from './MultiviewClient';
 
 export const metadata: Metadata = {
   title: '멀티뷰 | SMEB Archive',
-  description: 'SOOP 라이브 방송을 최대 6개까지 동시에 시청하세요',
+  description: 'SOOP 라이브 방송을 최대 4개까지 동시에 시청하세요',
 };
 
 export default function MultiviewPage() {
